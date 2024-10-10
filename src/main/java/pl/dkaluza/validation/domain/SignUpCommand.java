3805f6efa905dff10bf8d817b5114f373c4c0f98
Dawid Kałuża <dkaluza27@gmail.com>
2024-10-10 09:54:47 +0200

    add validation

--- src/main/java/pl/dkaluza/validation/domain/SignUpCommand.java
+++ src/main/java/pl/dkaluza/validation/domain/SignUpCommand.java
@@ -1,6 +1,9 @@
 package pl.dkaluza.validation.domain;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record SignUpCommand(
+    @NotBlank
     String name
 ) {
 }
--- src/main/java/pl/dkaluza/validation/domain/UserService.java
+++ src/main/java/pl/dkaluza/validation/domain/UserService.java
@@ -1,14 +1,16 @@
 package pl.dkaluza.validation.domain;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public User signUp(SignUpCommand signUpCommand) {
+    public User signUp(@Valid SignUpCommand signUpCommand) {
         var user = new User(1L, signUpCommand.name());
         logger.info("Sign up: {}", signUpCommand);
         return user;
